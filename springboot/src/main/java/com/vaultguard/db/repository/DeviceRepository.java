894de8423a9671a3fb2d1d0406fbda35d956ab87
Chuntung <ho@chuntung.com>
2026-03-29 07:57:20 +0800

    fix: add @Transactional to deleteByUserUuid

--- springboot/src/main/java/com/vaultguard/db/repository/DeviceRepository.java
+++ springboot/src/main/java/com/vaultguard/db/repository/DeviceRepository.java
@@ -2,11 +2,13 @@ package com.vaultguard.db.repository;
 
 import com.vaultguard.db.entity.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, String> {
     List<Device> findByUserUuid(String userUuid);
     Optional<Device> findByUserUuidAndName(String userUuid, String name);
+    @Transactional
     void deleteByUserUuid(String userUuid);
 }
